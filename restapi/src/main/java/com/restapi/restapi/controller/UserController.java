d11f6474d7e6a256507020892aaea9d21aea0ae1
Nebi Gul <nebi.gul@etiya.com>
2023-03-29 21:53:47 +0300

    neccesery details

--- restapi/src/main/java/com/restapi/restapi/controller/UserController.java
+++ restapi/src/main/java/com/restapi/restapi/controller/UserController.java
@@ -23,7 +23,6 @@ public class UserController {
         User thirtUser = new User("03", "Taner", "YILDIRIM", "taneryildirim", 28);
          users = Arrays.asList(firstUser, secondUser,thirtUser);
     }
-
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return new ResponseEntity<>(users, OK); // static import
@@ -31,6 +30,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable String id) {
+        /* en: We are opening a stream and it is filtering for me.
+        If any user id is equal to the given id, add the first one to the result.
+        Possible error throwing
+        tr: Bir akış açıyoruz ve benim için filtreleme yapıyor.
+        Herhangi bir kullanıcı kimliği verilen kimliğe eşitse,
+        sonuca ilkini ekleyin ve sonuç yoksa  olası hata atma
+         */
         User result = users.stream()
                 .filter( user -> user.getId().equals(id))
                 .findFirst().orElseThrow(()->new RuntimeException("User not found"));
